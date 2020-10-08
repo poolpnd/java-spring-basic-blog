91c622429342be96a5e2365f431a7c0c39e04c6e
seunghyun baek <seunghyun.baek2@t-mobile.com>
2020-10-08 14:48:37 -0400

    finishing first 2
    gogo

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,16 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 public class BlogController {
-
+    @RequestMapping("/")
+    public String listPosts(ModelMap map){
+        map.put("title", "Blog post 1");
+        return "home";
+    }
 }
