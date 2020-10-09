d30581207ba1596b8ec0bfef73d2dd6f2672ba08
seunghyun baek <seunghyun.baek2@t-mobile.com>
2020-10-08 22:51:37 -0400

    assingment3
    added

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,14 +1,29 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
+
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap){
-        modelMap.put("title", "Blog Post 1");
+
+       List<Post> post = this.postRepository.getAllPosts();
+        //modelMap.put("title", "Blog Post 1");
+        //modelMap.putAll("posts",post);
+        modelMap.put("posts", post);
         return "home";
     }
 }
